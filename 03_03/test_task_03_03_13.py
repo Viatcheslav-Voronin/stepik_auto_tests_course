9097fdd35d143872d969e6f24bbacea5dd05eff2
Viatcheslav Voronin <viatcheslav.voronin@gmail.com>
2025-01-11 09:49:33 +0300

    Задания раздела 3.3 "Тестирование с помощью PyTest"

---
+++ 03_03/test_task_03_03_13.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import unittest
+from time import sleep
+
+
+class TestRegistrationPages(unittest.TestCase):
+    def test_registration1(self):
+        url = 'http://suninjuly.github.io/registration1.html'
+        # Подключение браузера Chrome
+        driver = webdriver.Chrome()
+        # Открытие заданной страницы в браузере
+        driver.get(url)
+        # Заполнение полей формы
+        driver.find_element(By.CSS_SELECTOR, ".first_block input.second").send_keys("Last name")
+        driver.find_element(By.CSS_SELECTOR, ".first_block input.first").send_keys("First name")
+        driver.find_element(By.CSS_SELECTOR, ".first_block input.third").send_keys("email@site.com")
+        driver.find_element(By.CSS_SELECTOR, ".second_block input.first").send_keys("Phone")
+        driver.find_element(By.CSS_SELECTOR, ".second_block input.second").send_keys("Address")
+        # Нажимаем кнопку отправки данных формы
+        driver.find_element(By.CLASS_NAME, 'btn-default').click()
+        # Пауза перед получением результата
+        sleep(3)
+        # Получение значения текста элемента
+        welcome_text = driver.find_element(By.TAG_NAME, "h1").text
+        # Пауза перед закрытием браузера
+        sleep(10)
+        # Закрытие браузера
+        driver.quit()
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+    def test_registration2(self):
+        url = 'http://suninjuly.github.io/registration2.html'
+        # Подключение браузера Chrome
+        driver = webdriver.Chrome()
+        # Открытие заданной страницы в браузере
+        driver.get(url)
+        # Заполнение полей формы
+        driver.find_element(By.CSS_SELECTOR, ".first_block input.second").send_keys("Last name")
+        driver.find_element(By.CSS_SELECTOR, ".first_block input.first").send_keys("First name")
+        driver.find_element(By.CSS_SELECTOR, ".first_block input.third").send_keys("email@site.com")
+        driver.find_element(By.CSS_SELECTOR, ".second_block input.first").send_keys("Phone")
+        driver.find_element(By.CSS_SELECTOR, ".second_block input.second").send_keys("Address")
+        # Нажимаем кнопку отправки данных формы
+        driver.find_element(By.CLASS_NAME, 'btn-default').click()
+        # Пауза перед получением результата
+        sleep(3)
+        # Получение значения текста элемента
+        welcome_text = driver.find_element(By.TAG_NAME, "h1").text
+        # Пауза перед закрытием браузера
+        sleep(10)
+        # Закрытие браузера
+        driver.quit()
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
